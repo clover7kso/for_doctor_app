a0f6ef69323189adbc58f7f3c1a3bc7d4362df43
woony1 <woony@luxrobo.com>
2020-09-09 16:27:25 +0900

    Custom TabNavigator

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -5,13 +5,49 @@ import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Profile from "../screens/Profile";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { Platform } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+          style: { height: 60 },
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let iconColor;
+            if (route.name === "Home") {
+              iconName = Platform.OS === "ios" ? "ios-home" : "md-home";
+              iconColor = focused ? "#000000" : "#F3F3F3";
+            } else if (route.name === "Search") {
+              iconName = Platform.OS === "ios" ? "ios-calendar" : "md-calendar";
+              iconColor = focused ? "#000000" : "#F3F3F3";
+            } else if (route.name === "Notifications") {
+              iconName = focused
+                ? Platform.OS === "ios"
+                  ? "ios-mail-open"
+                  : "md-mail-open"
+                : Platform.OS === "ios"
+                ? "ios-mail"
+                : "md-mail";
+
+              iconColor = focused ? "#000000" : "#F3F3F3";
+            } else if (route.name === "Profile") {
+              iconName = Platform.OS === "ios" ? "ios-person" : "md-person";
+              iconColor = focused ? "#000000" : "#F3F3F3";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={32} color={iconColor} />;
+          },
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Search" component={Search} />
         <Tab.Screen name="Notifications" component={Notifications} />
