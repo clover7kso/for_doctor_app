634956ba717902e20033db8c4196a58cfd58ee7d
woony <clover7kso@naver.com>
2020-09-29 22:22:38 +0900

    Fix

--- hooks/useValue.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const useValue = (intialValue) => {
-  const [value, setValue] = useState(intialValue);
-  return { value, setValue };
-};
-
-export default useValue;
