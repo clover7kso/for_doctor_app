d7244603a7c8e00adf9958615da8b2b917af7b2a
Woony <clover7kso@naver.com>
2020-11-23 14:01:38 +0900

    Mac book Setting

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://192.168.35.5:4000';
-export const WEB_SOCKET_URL = 'ws://192.168.35.5:4000/';
+export const SERVER_URL = 'http://192.168.123.104:4000';
+export const WEB_SOCKET_URL = 'ws://192.168.123.104:4000/';
